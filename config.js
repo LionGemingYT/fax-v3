6fcf822c2a0a5b83cec96b85f8fa2ffcb2b83822
LionGamingYT-acc <msultanutomo@gmail.com>
2022-10-25 20:53:06 +0700

    fax-v3

---
+++ config.js
@@ -0,0 +1,27 @@
+userbot = {
+ owner: [ "6282136821559" ],
+  MONGO_URI: "mongodb+srv://LionGamingYT:<password>@cluster0.zyec3vp.mongodb.net/?retryWrites=true&w=majority",
+   mess: {
+   wait: "tunggu sedang di proses...",
+   error: "errorr!!"
+   },
+   prefix: ["/"],
+   gexp: 50,
+   limit: 100,
+   eror: "Sepertinya Eror",
+   waits: "Sedang di proses",
+   butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
+   packname: "SELFBOT",
+   author: 'SultanGanz',
+    setting: {
+    admin: "only admin",
+    group: "only group",
+    owner: "owner only pack",
+    jadibot: "jadibot only",
+    botadmin: "bot harus menjadi admin",
+    nsfw: "FITUR BELUM DIAKTIFKAN!"
+    }
+}
+module.exports = userbot
+
+
